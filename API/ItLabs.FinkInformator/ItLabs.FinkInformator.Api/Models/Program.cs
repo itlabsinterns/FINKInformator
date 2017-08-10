a604345409eccc01bed65314827dadef4984f475
aleksandra.bozinoska <aleksandra.bozinoska@students.finki.ukim.mk>
2017-08-10 13:09:54 +0200

    Program class updated

--- API/ItLabs.FinkInformator/ItLabs.FinkInformator.Api/Models/Program.cs
+++ API/ItLabs.FinkInformator/ItLabs.FinkInformator.Api/Models/Program.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ItLabs.FinkInformator.Api.Models
 {
+   public class Program
+    {
+        [Required]
+        public int ProgramId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string ProgramName { get; set; }
+       
+    }
 }
