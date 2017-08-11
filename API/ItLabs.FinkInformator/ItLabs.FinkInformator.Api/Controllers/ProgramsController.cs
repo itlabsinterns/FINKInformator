b20e2ffdcdb383673eae8441d491c46b24871d5f
aleksandra.bozinoska <aleksandra.bozinoska@students.finki.ukim.mk>
2017-08-11 15:23:41 +0200

    Revert "Revert "ProgramsController GET method added and ProgramsResponse class added""
    This reverts commit 8c913e3fc5fbfcd6cfb42cf233f9ebd0a72d8c2e.

---
+++ API/ItLabs.FinkInformator/ItLabs.FinkInformator.Api/Controllers/ProgramsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using ItLabs.FinkInformator.Api.Responses;
+
+
+namespace ItLabs.FinkInformator.Api.Controllers
+{
+    public class ProgramsController: ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            ProgramsResponse Response = new ProgramsResponse();
+            if(!Response.IsSuccessful)
+            {
+                return BadRequest(Response.ToString());
+            }
+            return Ok(Response.Programs);
+        }
+
+    }
+}
---
+++ API/ItLabs.FinkInformator/ItLabs.FinkInformator.Api/Responses/ProgramsResponse.cs
@@ -0,0 +1,40 @@
+﻿using ItLabs.FinkInformator.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ItLabs.FinkInformator.Api.Responses
+{
+    public class ProgramsResponse : ResponseBase
+    {
+        //property containing all Program names
+        public List<string> Programs;
+
+        //Class constructor, setting Programs property to null
+        public ProgramsResponse() : base()
+        {
+            this.Programs = null;
+            GetAllPrograms();
+        }
+
+        //Method setting Programs property
+        private void GetAllPrograms()
+        {
+            SchoolContext schoolContext = new SchoolContext();
+
+            this.Programs = (from p in schoolContext.Programs
+                              select p.ProgramName)
+                              .ToList();
+                       
+
+            if (Programs == null || Programs.Count==0)
+            {
+                this.Errors.Add("No Result!");
+                this.IsSuccessful = false;
+            }
+        }
+
+
+    }
+}
