b88ec736aabd374fe4c28de3132a4e92636fb24e
danilo.popovikj <danilo.popovikj@DESKTOP-J35K87O>
2017-08-15 15:58:30 +0200

    Added GetCoursePrerequisitesResponse Added Method for getting courses prerequisites based on that course

---
+++ API/ItLabs.FinkInformator/ItLabs.FinkInformator.Api/Responses/GetCoursePrerequisitesResponse.cs
@@ -0,0 +1,17 @@
+﻿using ItLabs.FinkInformator.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ItLabs.FinkInformator.Api.Responses
+{
+    public class GetCoursePrerequisitesResponse:ResponseBase
+    {
+        public GetCoursePrerequisitesResponse()
+        {
+            Prerequisites = new List<Course>();
+        }
+        public List<Course> Prerequisites { get; set; }
+    }
+}
