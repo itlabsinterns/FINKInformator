221430fcb871e7188a6c5aaec6265f718a0ccce2
kostadin.kadiev@it-labs.com <kostadin.kadiev@it-labs.com>
2017-08-21 10:13:16 +0200

    UI script review

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -18,22 +18,26 @@ $(document).ready(function () {
             url: "http://localhost:4329/programs/" + program + "/" + forRequest,
             dataType: 'json',
             success: function (data) {
-                $("#Zadolzitelni").append('<a style="text-align:center; color: white !important; background-color: dimgray!important;" id="zad" class="list-group-item forli"><b>ЗАДОЛЖИТЕЛНИ ПРЕДМЕТИ</b></a>')
-                $("#Izborni").append('<a style="text-align:center; color: white !important; background-color: dimgray!important;" id="zad" class="list-group-item forli"><b>ИЗБОРНИ ПРЕДМЕТИ</b></a>')
+                $("#Zadolzitelni").append('<a style="text-align:center; color: white !important; background-color: dimgray!important;" id="zad" class="list-group-item forli"><b>ЗАДОЛЖИТЕЛНИ ПРЕДМЕТИ</b></a>');
+
+                $("#Izborni").append('<a style="text-align:center; color: white !important; background-color: dimgray!important;" id="zad" class="list-group-item forli"><b>ИЗБОРНИ ПРЕДМЕТИ</b></a>');
+
                 $.each(data, function (i, item) {
-                    if (item.IsMandatory == true) {
-                        $("#Zadolzitelni").append('<a id="' + item.Course.CourseId + '" class="list-group-item forli ">' + item.Course.CourseName + '</a>')
-                    } else {
-                        $("#Izborni").append('<a id=' + item.Course.CourseId + ' class="list-group-item forli ">' + item.Course.CourseName + '</a>')
-                    
-                    }    
-                })
+                    var course = $('<a id="' + item.Course.CourseId + '" class="list-group-item forli ">' + item.Course.CourseName + '</a>');
+
+                    if (item.IsMandatory)
+                        $("#Zadolzitelni").append(course);
+                    else
+                        $("#Izborni").append(course);
+                });
+
                 $("#Zadolzitelni").show();
-                if($("#Izborni > a").length>1){
+                if ($("#Izborni > a").length > 1) {
                     $("#Izborni").show();
                 }
-             while($("#Zadolzitelni > a").length<=5){
-                $("#Zadolzitelni").append('<a class="list-group-item forli">-ИЗБОРЕН ПРЕДМЕТ-</a>');
+
+                while ($("#Zadolzitelni > a").length <= 5) {
+                    $("#Zadolzitelni").append('<a class="list-group-item forli">-ИЗБОРЕН ПРЕДМЕТ-</a>');
                 }
             }
         })
@@ -46,7 +50,7 @@ $(document).ready(function () {
             dataType: 'json',
             success: function (data) {
                 $.each(data.Programs, function (i, item) {
-                    $("#programsButtonContainer").append('<button programId=' + item.ProgramId + ' style="margin-bottom:4%;" type="button" class="btn btn-lg btn-block btn-danger" ><b>'+ item.ProgramName.toUpperCase() +'</b></button>')
+                    $("#programsButtonContainer").append('<button programId=' + item.ProgramId + ' style="margin-bottom:4%;" type="button" class="btn btn-lg btn-block btn-danger" ><b>' + item.ProgramName.toUpperCase() + '</b></button>')
                 })
             }
         })
@@ -57,58 +61,58 @@ $(document).ready(function () {
         {
             scrollTop: $("#" + div_id).offset().top
         },
-        2000);
+        1000);
     }
 
-    function errorDiv(message){
+    function errorDiv(message) {
         $("#error").remove();
-        $("#programsContainer").prepend('<div id="error" style="margin-top:8%;" class="alert alert-danger alert-dismissable fade in text-center"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> <strong>Предупредување! </strong>' 
+        $("#programsContainer").prepend('<div id="error" style="margin-top:8%;" class="alert alert-danger alert-dismissable fade in text-center"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> <strong>Предупредување! </strong>'
         + message + '</div>');
     }
 
-    function attachProgramEvents(){
-        $("#programsButtonContainer").on("click",".btn",function(){
+    function attachProgramEvents() {
+        $("#programsButtonContainer").on("click", ".btn", function () {
             program = $(this).attr("programId");
             scroll_to_div('yearContainer');
         })
 
     }
 
-    function attachEvents(){
+    function attachEvents() {
         $(".godina").click(function () {
             if (program == 0) {
                 scroll_to_div('programsContainer');
                 errorDiv("Не избравте смер");
-            }else{
+            } else {
                 scroll_to_div('semesterContainer');
                 year = $(this).attr("year");
             }
         })
 
-        $("#semesterButtonContainer").on("click",".btn",(function () {
-            if(year == 0 && program == 0){
+        $("#semesterButtonContainer").on("click", ".btn", (function () {
+            if (year == 0 && program == 0) {
                 errorDiv("Не избравте смер и година");
                 scroll_to_div('programsContainer');
             }
-            else if (year == 0){
+            else if (year == 0) {
                 errorDiv("Не избравте година");
                 scroll_to_div('programsContainer');
             }
-            else if (program == 0){
+            else if (program == 0) {
                 errorDiv("Не избравте смер");
                 scroll_to_div('programsContainer');
-            }else{
+            } else {
                 semester = $(this).attr("semester");
-                console.log(year+" "+semester);
+                console.log(year + " " + semester);
                 var final = (year * 2) + (semester - 2);
                 $("#Zadolzitelni").empty();
                 $("#Izborni").empty();
-                getInfoAndFillLists(program,final );
+                getInfoAndFillLists(program, final);
                 scroll_to_div('coursesContainer');
             }
         }))
 
-        $("#coursesContainer").on("click","a",function(){
+        $("#coursesContainer").on("click", "a", function () {
             $.ajax({
                 type: 'GET',
                 url: "http://localhost:4329/courses/" + this.id,
