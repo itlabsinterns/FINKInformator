ecbe355dc65a01bbb925b58a4f48d2f3e9e32cd8
jovica ilievski <jovicailievski97@gmail.com>
2017-08-25 08:55:34 +0200

    test

--- UI-Angular/FinkInformator/src/app/app.module.ts
+++ UI-Angular/FinkInformator/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NavbarComp } from './navbar/navbar.component';
+import { NavbarsComponent } from './navbars/navbars.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComp,
+    NavbarsComponent
   ],
   imports: [
     BrowserModule
